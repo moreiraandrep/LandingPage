1c4a9f4594c6c19e4db6e71bbe1b9b8fd282522f
moreiraandrep <moreira.andre.p@gmail.com>
2022-01-28 01:52:38 -0300

    Sessoes Depoimentos, Contato e Footer. Finalização

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //Abre e fecha o menu quando clicar nos icones
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
-
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show') //adiciona ou remove a classe show
@@ -10,7 +9,6 @@ for (const element of toggle) {
 
 //Quando clicar em um icone do menu, fechar o menu
 const links = document.querySelectorAll('nav ul li a')
-
 for (const link of links) {
   link.addEventListener('click', function () {
     nav.classList.remove('show')
@@ -20,8 +18,7 @@ for (const link of links) {
 //aplicar box-shadow no menu quando der scroll na página
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
+function changeHeaderScroll() {
   if (window.scrollY >= navHeight) {
     //maior que a altura do header
     header.classList.add('scroll')
@@ -29,16 +26,22 @@ window.addEventListener('scroll', function () {
     //menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
-//Swiper corrocel
+//Swiper / carousel / slide
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
-  mousewheel: true,
-  keyboard: true
+  mousewheel: false,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
@@ -54,6 +57,50 @@ scrollReveal.reveal(
   #about .image, #about .text, 
   #services .header, #services .card, 
   #testimonials .header, #testimonials .testimonials, 
-  #contact .text, #contact .links`,
+  #contact .text, #contact .links,
+  footer .brand, footer .social`,
   { interval: 100 }
 )
+
+//Botão voltar ao topo
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+//Menu ativo
+//Seleciona todas as section que contém um ID
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+//Adiciona o evento de scroll na tela
+window.addEventListener('scroll', function () {
+  changeHeaderScroll()
+  backToTop()
+  activateMenuCurrentSection()
+})
